26cb481058959e8a38377ed7ac0dab1b285f9ea8
xyz <samikshabudhale80@gmail.com>
2026-02-01 16:46:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,66 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const audio = document.getElementById("background-music");
-    const startBtn = document.getElementById("start-btn");
-    const yesBtn = document.getElementById("yes-btn");
-    const noBtn = document.getElementById("no-btn");
-    const choiceBox = document.querySelector(".choice-box");
-    const questionText = document.getElementById("question-text");
-    const threedBox = document.querySelector(".threed-box");
-    const volumeSlider = document.getElementById("volume-slider");
-    const muteBtn = document.getElementById("mute-button");
-    const icon = muteBtn.querySelector("i");
+  const audio = document.getElementById("background-music");
 
-    let partnerName = "PIYU";
-    let noClicks = 0;
+  const startBtn = document.getElementById("start-btn");
+  const yesBtn = document.getElementById("yes-btn");
+  const noBtn = document.getElementById("no-btn");
 
-    // Volume control
+  const questionText = document.getElementById("question-text");
+  const choiceBox = document.querySelector(".choice-box");
+  const threedBox = document.querySelector(".threed-box");
+
+  const volumeSlider = document.getElementById("volume-slider");
+  const muteBtn = document.getElementById("mute-button");
+  const icon = muteBtn.querySelector("i");
+
+  let partnerName = "PIYU";
+  let noCount = 0;
+
+  // Volume
+  audio.volume = volumeSlider.value;
+  volumeSlider.addEventListener("input", () => {
     audio.volume = volumeSlider.value;
+  });
 
-    volumeSlider.addEventListener("input", () => {
-        audio.volume = volumeSlider.value;
-    });
+  muteBtn.addEventListener("click", () => {
+    audio.muted = !audio.muted;
+    icon.className = audio.muted ? "fa fa-volume-off" : "fa fa-volume-up";
+  });
 
-    muteBtn.addEventListener("click", () => {
-        audio.muted = !audio.muted;
-        icon.className = audio.muted ? "fa fa-volume-off" : "fa fa-volume-up";
-    });
+  // Start
+  startBtn.addEventListener("click", () => {
+    audio.play().catch(() => {});
+    startBtn.style.display = "none";
+    choiceBox.classList.remove("hide");
 
-    // Start interaction
-    startBtn.addEventListener("click", () => {
-        audio.play().catch(()=>{});
+    questionText.innerHTML = `
+      <span class="partner-name">${partnerName}</span><br>
+      You and me, Valentine style?
+    `;
+  });
 
-        startBtn.style.display = "none";
-        choiceBox.classList.remove("hide");
+  // YES
+  yesBtn.addEventListener("click", () => {
+    questionText.innerHTML = `
+      <span class="partner-name">${partnerName}</span><br>
+      <span class="love-text">Looks like a plan 💖</span><br>
+      <span class="love-text">I knew you couldn’t resist 😏</span>
+    `;
+    choiceBox.style.display = "none";
+    threedBox.classList.remove("hide");
+  });
 
-        questionText.innerHTML = `
-            <span class="partner-name">${partnerName}</span><br>
-            You and me, Valentine style?
-        `;
-    });
+  // NO (cute, not aggressive)
+  noBtn.addEventListener("click", () => {
+    noCount++;
 
-    // YES
-    yesBtn.addEventListener("click", () => {
-        questionText.innerHTML = `
-            <span class="partner-name">${partnerName}</span><br>
-            <span class="love-text">Knew it 😌💖</span><br>
-            <span class="love-text">You’re stuck with me now</span>
-        `;
-        choiceBox.style.display = "none";
-        threedBox.classList.remove("hide");
-    });
+    if (noCount <= 2) {
+      questionText.innerHTML += `<br><em>Bold choice 😌</em>`;
+      noBtn.style.transform = `translateX(${Math.random() * 80 - 40}px)`;
+    } else {
+      noBtn.style.transform = `translateX(${Math.random() * 140 - 70}px)`;
+      yesBtn.style.transform = "scale(1.08)";
+    }
+  });
 
-    // NO (cute frustration)
-    noBtn.addEventListener("click", () => {
-        noClicks++;
-
-        if (noClicks <= 2) {
-            questionText.innerHTML += `<br><em>Bold choice 🤨</em>`;
-            noBtn.style.transform = `translateX(${Math.random()*120 - 60}px)`;
-        } else {
-            noBtn.style.transform = `translateX(${Math.random()*200 - 100}px)`;
-            yesBtn.style.transform = "scale(1.1)";
-        }
-    });
-    
 });
